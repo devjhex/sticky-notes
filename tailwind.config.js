bed218e94e3181bf6105ddeff05c62c6e6dc6bb9
Jonah Ssegawa <jonahssegawa44532@gmail.com>
2023-08-28 16:33:44 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    'notes.html',
+    'singleNote.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        nunito:"nunito"
+      }
+    },
+  },
+  plugins: [],
+}
+
